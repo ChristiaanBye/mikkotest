14c4ad0c694cf09c57429656924111a937118cce
Christiaan Bye <c.bye@unitrust.nl>
2019-12-02 19:57:22 +0100

    Created integration test for .csv outputs, made small modifications to existing integration tests

--- tests/integration/Command/PaymentDatesTest.php
+++ tests/integration/Command/PaymentDatesTest.php
@@ -5,6 +5,7 @@ namespace MikkoTest\Tests\Integration\Command;
 
 use Carbon\Carbon;
 use MikkoTest\Command\PaymentDates;
+use MikkoTest\Outputter\CsvOutputter;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Console\Tester\CommandTester;
 
@@ -23,6 +24,58 @@ class PaymentDatesTest extends TestCase
         $command = new PaymentDates();
 
         $this->commandTester = new CommandTester($command);
+
+        // For all tests we will assume the current date in June 1999
+        Carbon::setTestNow(Carbon::create(1999, 6, 15));
+    }
+
+    protected function tearDown(): void
+    {
+        // Restore to the present after testing
+        Carbon::setTestNow();
+    }
+
+    public function test_csvOutput_writesOutputForGivenYear()
+    {
+        $this->commandTester->execute(
+            array(
+                '-f' => 'csvOutput_writesOutputForGivenYear.csv',
+                '-y' => '2010'
+            )
+        );
+
+        $this->assertStringContainsString(
+            'Outputted payment dates to "output/csvOutput_writesOutputForGivenYear.csv',
+            $this->commandTester->getDisplay()
+        );
+
+        $this->assertSame(
+            file_get_contents(__DIR__ . '/resources/expectedOutput_outputForGivenYear.csv'),
+            file_get_contents(CsvOutputter::DEFAULT_OUTPUT_DIRECTORY . 'csvOutput_writesOutputForGivenYear.csv')
+        );
+
+        unlink(CsvOutputter::DEFAULT_OUTPUT_DIRECTORY . 'csvOutput_writesOutputForGivenYear.csv');
+    }
+
+    public function test_csvOutput_writesOutputForCurrentYear()
+    {
+        $this->commandTester->execute(
+            array(
+                '-f' => 'csvOutput_writesOutputForCurrentYear.csv'
+            )
+        );
+
+        $this->assertStringContainsString(
+            'Outputted payment dates to "output/csvOutput_writesOutputForCurrentYear.csv',
+            $this->commandTester->getDisplay()
+        );
+
+        $this->assertSame(
+            file_get_contents(__DIR__ . '/resources/expectedOutput_outputForCurrentYear.csv'),
+            file_get_contents(CsvOutputter::DEFAULT_OUTPUT_DIRECTORY . 'csvOutput_writesOutputForCurrentYear.csv')
+        );
+
+        unlink(CsvOutputter::DEFAULT_OUTPUT_DIRECTORY . 'csvOutput_writesOutputForCurrentYear.csv');
     }
 
     public function test_dryRunOutput_providesOutputForGivenYear()
@@ -35,16 +88,13 @@ class PaymentDatesTest extends TestCase
         );
 
         $this->assertSame(
-            file_get_contents(__DIR__ . '/resources/expectedOutput_dryRunOutput_providesOutputForGivenYear.txt'),
+            file_get_contents(__DIR__ . '/resources/expectedOutput_outputForGivenYear.csv'),
             $this->commandTester->getDisplay()
         );
     }
 
     public function test_dryRunOutput_providesOutputForCurrentYear()
     {
-        // Let's return to June in 1999 for this test
-        Carbon::setTestNow(Carbon::create(1999, 6, 15));
-
         $this->commandTester->execute(
             array(
                 '--dry-run' => true
@@ -52,11 +102,8 @@ class PaymentDatesTest extends TestCase
         );
 
         $this->assertSame(
-            file_get_contents(__DIR__ . '/resources/expectedOutput_dryRunOutput_providesOutputForCurrentYear.txt'),
+            file_get_contents(__DIR__ . '/resources/expectedOutput_outputForCurrentYear.csv'),
             $this->commandTester->getDisplay()
         );
-
-        // ... and we are now back in the present
-        Carbon::setTestNow();
     }
 }
